ad59d54fa589c6b9091c3c4cef733b711ed9f149
focsik <focsick@icloud.com>
2023-01-03 22:13:44 +0300

    add code

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string ask(string question)
+{
+    Console.Write(question);
+    return Console.ReadLine();
+}
+
+int M = int.Parse(ask("Введите количеcтво строк: "));
+string[] strings = new string[M];
+for (int i = 0; i < strings.Length; i++) strings[i] = ask($"Введите строку №{i + 1}: ");
+
+List<string> output = new List<string>();
+
+for (int i = 0; i < strings.Length; i++)
+{
+    int len = strings[i].Length;
+    if (len <= 3)
+    {
+        output.Add(strings[i]);
+    }
+}
+
+Console.WriteLine("[{0}]", string.Join(", ", output));
